edef6a2df663aaf156ae6bef91a2630b4752f79b
Hasani <seyedmostafahassani@gmail.com>
2014-12-24 23:39:16 +0330

    js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	var bool = true;
 	if($(window).width()>768){
 		$('.collection1 .row  .left').height($('.collection1 .row  .right').outerHeight());
 	}else{
@@ -6,12 +7,15 @@ $(document).ready(function() {
 	}
 
 	$(window).scroll(function(event) {
-		if($(this).scrollTop() >= 300){
-			$('.navbar-fixed-top').addClass('shownavbar');
-		}else{
-			$('.navbar-fixed-top').removeClass('shownavbar');
-		}
-		
+		if($(window).width() >= 768){
+			if($(this).scrollTop() >= 300){
+				$('.navbar-fixed-top').addClass('shownavbar');
+				$('.navbar-toggle').css('top', 100+'px');
+			}else{
+				$('.navbar-fixed-top').removeClass('shownavbar');
+				$('.navbar-toggle').css('top', 10+'px');
+			}
+		}		
 	});
 
 	// $('.carousel').carousel({
@@ -38,5 +42,33 @@ $(document).ready(function() {
 				$('.collection1 .row  .left').height('85');
 			}
 			
+		});
+
+		$('.hiddenmenu li').on('click', function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+
+			$(this).children('ul').slideToggle('400');
+
+		});
+
+		if($(window).width()<768){
+			var top = $('.navbar-fixed-top').outerHeight();
+			$('.collection').css('margin-top', top+'px');
+			$('.navbar-toggle').css('top', 190+'px');
+		}else{
+			$('.collection').css('margin-top', -125+'px');
+			$('.navbar-toggle').css('top', 10+'px');
+		}
+
+		$(window).resize(function(event) {
+			if($(window).width()<768){
+				var top = $('.navbar-fixed-top').outerHeight();
+				$('.collection').css('margin-top', top+'px');
+				$('.navbar-toggle').css('top', 190+'px');
+			}else{
+				$('.collection').css('margin-top', -125+'px');
+				$('.navbar-toggle').css('top', 10+'px');
+			}
 		});
 });
